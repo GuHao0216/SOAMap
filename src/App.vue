93cb6778dd7f2894be838f02c8a9da4c99ba1a16
Sumixer@GH <1272639290@qq.com>
2020-03-21 13:26:00 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-      <router-view></router-view>  
-          
+      <router-view></router-view>
+
     <!-- <iframe allowtransparency="true" frameborder="0" width="290" height="96" scrolling="no"
         src="//tianqi.2345.com/plugin/widget/index.htm?s=1&z=1&t=0&v=0&d=2&bd=0&k=&f=&ltf=009944&htf=cc0000&q=1&e=1&a=1&c=54511&w=290&h=96&align=center"></iframe> -->
   </div>
@@ -13,6 +13,7 @@
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'app',
   components: {
     // HelloWorld
